756ced256aabe4f1318bd9427d440b8d22de8bb2
jmansdorfer <jmansdorfer19@gmail.com>
2024-11-21 15:44:10 -0500

    adding extra models example

--- examples/models.rs
+++ examples/models.rs
@@ -8,11 +8,21 @@ use pg_client::{client, models};
 async fn main() {
     let clt = client::Client::new().expect("client value");
 
+    // Models request will return all models if set to None
+    let req = models::Request::new(
+        None,
+    );
+
+    let result = clt.models(Some(&req)).await.expect("error from all models");
+
+    println!("\n\nall models response:\n{:?}\n\n", result);
+
+    // Models request will return only models for that capability if set
     let req = models::Request::new(
         Some("chat-completion".to_string()),
     );
 
-    let result = clt.models(Some(&req)).await.expect("error from models");
+    let result = clt.models(Some(&req)).await.expect("error from chat-completion models");
 
-    println!("\n\nmodels response:\n{:?}\n\n", result);
+    println!("\n\nchat-completion models response:\n{:?}\n\n", result);
 }
